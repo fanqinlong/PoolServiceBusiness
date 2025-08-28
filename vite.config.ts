cc1ec4c76b4bcc20a4c5aee927cad723a6f5f4cc
fanqinlong <fanqinlong@gmail.com>
2025-08-28 02:14:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/PoolServiceClient/',
+    base: '/PoolServiceBusiness/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
